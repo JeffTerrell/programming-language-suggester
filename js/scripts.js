8ff39d6e0e99464f1773b7811e22b85172b11cbe
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-22 19:06:12 -0400

    BUGFIX: Fixed issue with form selection not inputting and counting towards variable in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Business Logic
 let js = 0;
-console.log(js);
 let python = 0;
 let csharp = 0;
 
@@ -18,14 +17,13 @@ function scoreKeeper(language) {
 
 function mostPoints(js,python,csharp) {
   if ("fashionstyle" === "select" || "preference" === "select" || "fantasycreature" === "select" || "music" === "select" || "pickcolor" === "select") {
-    $("#error").show();
+    return $("#error").show();
   } else if (js > python && js > csharp) {
-    $("#outputjs").show();
-    console.log("help!");
+    return $("#outputjs").show();
   } else if (python > js && python > csharp) {
-    $("#outputpython").show();
+    return $("#outputpython").show();
   } else if (csharp > js && csharp > python) {
-    $("#outputcsharp").show();
+    return $("#outputcsharp").show();
   }
 }
 
@@ -37,20 +35,22 @@ $(document).ready(function() {
   $("#programselector").submit(function(event) {
     event.preventDefault();
 
-    // $('#fashionstyle').val();
-    // const test = $("#fashionstyle option:selected").val();
-    scoreKeeper($('fashionstyle').find(":selected").val());
-    console.log(js);
+    scoreKeeper($("#fashionstyle").val());  
     scoreKeeper($("#preference").val());
     scoreKeeper($("#fantasycreature").val());
     scoreKeeper($("#music").val());
     scoreKeeper($("#pickcolor").val());
+   
 
-    mostPoints(js, python, csharp);
+    mostPoints(js, python, csharp)
 
     $("#outputjs").empty();
     $("#outputpython").empty();
     $("#outputcsharp").empty(); 
+
+    js = 0
+    lisa = 0
+    nelson = 0
 
   });
 });    
