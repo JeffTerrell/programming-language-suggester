2e26bb30907e536e2148e188c1a1d84132695e14
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-22 16:22:13 -0400

    WIP: working business logic to keep track of variable score to display a winner

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@ const js = 0;
 const python = 0;
 const csharp = 0;
 
-
+function scorekeeper(language) {
+  if (language === "js") {
+    js += 1;
+  } else if (language === "python") {
+    python += 1;
+  } else if (language === "csharp") {
+    csharp += 1;
+  }
+}
 
 
 
