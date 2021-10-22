8d8c7c01f8c01ec74923778b091f543ab3f4d80b
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-22 15:57:00 -0400

    correct form values to reflect added JS variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
+// Business Logic
+const js = 0;
+const python = 0;
+const csharp = 0;
+
+
+
+
+
+
+// User Interface Logic
 $(document).ready(function() {
   $("#programselector").submit(function(event) {
     event.preventDefault();
+    $("#outputjs").empty();
+    $("#outputpython").empty();
+    $("#outputcsharp").empty();
+ 
 
   });
 });    
